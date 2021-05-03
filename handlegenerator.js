c60d25895e25d2c367196184fe40005be62c18bc
Juan Andrés Caycedo S <ja.caycedo@uniandes.edu.co>
2021-05-02 21:05:33 -0500

    ACBE

--- handlegenerator.js
+++ handlegenerator.js
@@ -1,87 +1,86 @@
-let jwt = require( 'jsonwebtoken' );
-let config = require( './config' );
+let jwt = require('jsonwebtoken');
+let config = require('./config');
 let md5 = require('md5')
 const { ROLE } = require('./Roles');
-vas [getUserByName, insertUser] = require("./dbpetitions")
+var [getUserByName, insertUser] = require("./dbpetitions")
 
 // Clase encargada de la creación del token
 class HandlerGenerator {
 
-  login( req, res ) {
+  login(req, res) {
     // Extrae el usuario y la contraseña especificados en el cuerpo de la solicitud
     let username = req.body.username;
     let password = req.body.password;
-    
+
     // Este usuario y contraseña, en un ambiente real, deben ser traidos de la BD
-    let mockedUsername = 'admin';
-    let mockedPassword = md5('password');
-
-    let xd = getUserByName(username);
-
-
-    // Si se especifico un usuario y contraseña, proceda con la validación
-    // de lo contrario, un mensaje de error es retornado
-    if( username && password ) {
-
-      // Si los usuarios y las contraseñas coinciden, proceda con la generación del token
+    
+    getUserByName(username).then(user => {
+      
+      let mockedUsername = user.username;
+      let mockedPassword = user.password;
+      // Si se especifico un usuario y contraseña, proceda con la validación
       // de lo contrario, un mensaje de error es retornado
-      if( username === mockedUsername && md5(password) === mockedPassword ) {
-        
-        // Se genera un nuevo token para el nombre de usuario el cuál expira en 24 horas
-        let token = jwt.sign( { username: username },
-          config.secret, { expiresIn: '24h' } );
-        
-        // Retorna el token el cuál debe ser usado durante las siguientes solicitudes
-        res.json( {
-          success: true,
-          message: 'Authentication successful!',
-          token: token
-        } );
+      if (username && password) {
+
+        // Si los usuarios y las contraseñas coinciden, proceda con la generación del token
+        // de lo contrario, un mensaje de error es retornado
+        if (username === mockedUsername && md5(password) === mockedPassword) {
+
+          // Se genera un nuevo token para el nombre de usuario el cuál expira en 24 horas
+          let token = jwt.sign({ username: username, role:user.role },
+            config.secret, { expiresIn: '24h' });
+
+          // Retorna el token el cuál debe ser usado durante las siguientes solicitudes
+          res.json({
+            success: true,
+            message: 'Authentication successful!',
+            token: token
+          });
+
+        } else {
+
+          // El error 403 corresponde a Forbidden (Prohibido) de acuerdo al estándar HTTP
+          res.status(403).send({
+            success: false,
+            message: 'Incorrect username or password'
+          });
+
+        }
 
       } else {
-        
-        // El error 403 corresponde a Forbidden (Prohibido) de acuerdo al estándar HTTP
-        res.send( 403 ).json( {
+
+        // El error 400 corresponde a Bad Request de acuerdo al estándar HTTP
+        res.status(400).send({
           success: false,
-          message: 'Incorrect username or password'
-        } );
+          message: 'Authentication failed! Please check the request'
+        });
 
       }
-
-    } else {
-
-      // El error 400 corresponde a Bad Request de acuerdo al estándar HTTP
-      res.send( 400 ).json( {
-        success: false,
-        message: 'Authentication failed! Please check the request'
-      } );
-
-    }
+    });
 
   }
-  register(req, res)
-  {
-    user = {username : req.body.username , password : md5(req.body.username), role : getRole(req.body.role)}
+  register(req, res) {
+    let rol;
+    switch (req.body.role) {
+      case 'admin': rol = ROLE.ADMIN; break;
+      case 'manager': rol = ROLE.MANAGER; break;
+      default: rol = ROLE.BASIC; break;
+    }
+    let user;
+    user = { username: req.body.username, password: md5(req.body.password), role: rol }
     insertUser(user);
+    res.status(200).send("Agregado")
   }
-  getRole(rol)
-  {
-    switch(rol)
-    {
-      case 'admin' : return ROLE.ADMIN; break;
-      case 'manager' : return ROLE.MANAGER; break;
-      default: console.log("No admin or manager, basic user"); return ROLE.BASIC; break;
-    }
-  }
-  index( req, res ) {
-    
+  index(req, res) {
+
     // Retorna una respuesta exitosa con previa validación del token
-    res.json( {
+    res.json({
       success: true,
       message: 'Index page'
-    } );
+    });
 
   }
 }
+
 
 module.exports = HandlerGenerator;
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ var router = express.Router();
 
 var HandlerGenerator = require("../handlegenerator.js");
 var middleware = require("../middleware.js");
-const {connect} = require('./db/mongoConexion')
+const {connect} = require('../db');
+const { ROLE } = require('../Roles.js');
 connect();
 
 HandlerGenerator = new HandlerGenerator();
 
 /* GET home page. */
 router.get('/', middleware.checkToken, HandlerGenerator.index);
-
+router.get('/admin', middleware.checkToken, middleware.checkRole(ROLE.ADMIN), HandlerGenerator.index);
+router.get('/mantain', middleware.checkToken, middleware.checkRole(ROLE.MANAGER), HandlerGenerator.index);
 router.post( '/login', HandlerGenerator.login);
-router.post('/register', HandlerGenerator.register);
+router.post('/register', middleware.checkToken, middleware.checkRole(ROLE.ADMIN), HandlerGenerator.register);
 
 module.exports = router;
